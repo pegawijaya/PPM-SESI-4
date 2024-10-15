3784879bac56f7b6d69fc31c7cbe449939d8b48d
pega wijaya <pega.wijaya_ti22@nusaputra.ac.id>
2024-10-15 22:06:23 +0700

    tugas 4

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import AppNavigator from './src/navigation/AppNavigator';
+// import {GestureHandlerRootView} from 'react-native-gesture-handler';
+
+const App = () => {
+  return (
+    <>
+      <AppNavigator />
+    </>
+  );
+};
+
+export default App;
